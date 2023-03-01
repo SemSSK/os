996bd564dcd55dd95a3f79c27c7bb0b23e639cbe
semssk <ssksem015@gmail.com>
2023-03-01 17:23:13 +0100

    Removed the standard library added a custom panic handler changed entry function form main to start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] //removes the usage of the standard library
+#![no_main] // tells the compiler to not look for a main function as an entry point
+use core::panic::PanicInfo;
+
+// custom panic handler as the standard library isn't available
+#[panic_handler]
+fn panic(_info:&PanicInfo) -> ! {
+    loop {}
+}
+
+// the start function is the entry point for the bootloader
+#[no_mangle] // no mangle stops the compiler from changing the name of the function
+pub extern "C" fn _start() -> ! { // extern c means that the function calls on the c runtime
+    loop {}
 }
