588f4d2c738721ef0489e0d67aee9781b8297667
semssk <ssksem015@gmail.com>
2023-03-01 19:51:55 +0100

    implemented of print/ln! for the vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 #![no_std] //Don't link the standard library
-#![no_main] // tells the compiler to not look for a main function as an entry point
+#![no_main] mod vga_buffer;
+
+// tells the compiler to not look for a main function as an entry point
 use core::{panic::PanicInfo};
 
 // custom panic handler as the standard library isn't available
 #[panic_handler]
-fn panic(_info:&PanicInfo) -> ! {
+fn panic(info:&PanicInfo) -> ! {
+    println!("{}",info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello,world!";
 
 // the start function is the entry point for the bootloader
 #[no_mangle] // no mangle stops the compiler from changing the name of the function
 pub extern "C" fn _start() -> ! { // extern c means that the function calls on the c runtime
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i,&byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello world{}","!");
+    panic!("Some message");
     loop {}
 }
